03b93a3e23f8dc72df70cdff17b59ff8dad188f0
jacqques <jacqques99@hotmail.com>
2023-03-28 21:04:42 +0200

    added a few blank pages, little css

---
+++ CV/CV/Pages/Freetime.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CV.Pages
+{
+    public class FreetimeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- CV/CV/wwwroot/js/site.js
+++ CV/CV/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+console.log("Loading js")
+$(".dropdown").hover(function () {
+    $(this).toggleClass("open");
+});
+
+
+console.log("fin")
